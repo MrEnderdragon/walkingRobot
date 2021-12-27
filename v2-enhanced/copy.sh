64deae1d94acdc255a7b1f892b11a6dc264b41c3
Alice Wang <awang@zscaler.com>
2021-12-26 16:54:35 -0800

    Add copy.sh

---
+++ v2-enhanced/copy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+scp UVdisp.py  root@10.0.10.116:/walking_robot/v2Good
+scp slic.py  root@10.0.10.116:/walking_robot/v2Good
+scp regMove.py  root@10.0.10.116:/walking_robot/v2Good
+scp obstacleDetect.py  root@10.0.10.116:/walking_robot/v2Good
+scp hough.py  root@10.0.10.116:/walking_robot/v2Good
+scp curves.py  root@10.0.10.116:/walking_robot/v2Good
+scp cameraProcess.py  root@10.0.10.116:/walking_robot/v2Good
+scp aStar.py  root@10.0.10.116:/walking_robot/v2Good
+scp rotAndMerge.py   root@10.0.10.116:/walking_robot/v2Good
+scp ?_instWalk*.py  root@10.0.10.116:/walking_robot/v2Good
+
+
